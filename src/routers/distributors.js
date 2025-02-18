6d97ce8c5229d1fabe99243e222d0833c1d7b16e
manjeetcoreteams <manjeet@coreteams.us>
2025-02-18 11:17:17 +0530

    changes

--- src/routers/distributors.js
+++ src/routers/distributors.js
@@ -4,7 +4,7 @@ const {verifyToken} = require('../middlewares/auth')
 
 //router.post('/create-distributor',verifyToken,distributorsc.createDistributor)
 //router.post('/update-distributor',verifyToken,distributorsc.updateDistributor)
-router.post('/createDistributor',verifyToken,distributorsc.createDistributor)
+router.post('/createDistributor',distributorsc.createDistributor)
 router.get('/get-manufacturers',verifyToken,distributorsc.getManufacturer)
 router.get('/get-stocksByManufacturer',verifyToken,distributorsc.getStocksByManufacturer)
 
--- src/routers/manufacturers.js
+++ src/routers/manufacturers.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 const manufacturerc = require('../controllers/manufacturers')
 const {verifyToken} = require('../middlewares/auth')
 
-router.post('/createManufacturer',verifyToken,manufacturerc.createManufacturer)
+router.post('/createManufacturer',manufacturerc.createManufacturer)
 router.post('/updateManufacturer',verifyToken,manufacturerc.updateManufacturer)
 router.post('/getManufacturer',verifyToken,manufacturerc.getManufacturer)
 router.get('/manufacturer-prchaseOrders',verifyToken,manufacturerc.prchaseOrders)
