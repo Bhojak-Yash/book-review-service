df79700bd55814150e48ff79590f0c26f930f63f
manjeetcoreteams <manjeet@coreteams.us>
2025-03-12 16:17:46 +0530

    Update distributorPanelRoutes.js

--- src/routers/distributorPanelRoutes.js
+++ src/routers/distributorPanelRoutes.js
@@ -1,7 +1,1 @@
-const router = require('express').Router()
-// const distributorPanelController = require('../controllers/distributorPanelController')
-const {verifyToken} = require('../middlewares/auth')
 
-// router.get('/Stats_1', verifyToken, distributorPanelController.countoforders)
-
-module.exports =router
