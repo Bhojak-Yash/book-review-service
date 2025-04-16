bf9ca27456e03c9185f0605c16250e40c18353b1
manjeetcoreteams <manjeet@coreteams.us>
2025-04-16 13:31:48 +0530

    chnages

--- src/services/ordersService.js
+++ src/services/ordersService.js
@@ -306,9 +306,12 @@ class OrdersService {
 
 
 if(updates.orderStatus === "Inward"){
+  // console.log('inwarddddddddd')
   if(order?.dataValues?.balance<order?.dataValues?.invAmt){
-    let sss= updates
-    sss.orderStatus==='Partially paid'
+    // console.log('bda haiiiiiiiiii')
+    let sss = updates
+    sss.orderStatus='Partially paid'
+    // console.log(sss)
     await this.db.orders.update(sss, { where: { id: orderId } });
   }
 }else{
