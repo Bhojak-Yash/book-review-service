b50737a836e0a9c294d2bf5995c092d33e275047
manjeetcoreteams <manjeet@coreteams.us>
2025-05-13 17:28:45 +0530

    changes

--- src/controllers/salesReport.js
+++ src/controllers/salesReport.js
@@ -5,7 +5,11 @@ exports.sales_report = async (req, res) => {
     try {
         const data = {...req.query,...req.user}
         const result = await salesReport.sales_report(data);
-        return res.json(result);
+        return res.status(200).json({
+            status:200,
+            message:'ok',
+            apiData:result[0]
+        });
     } catch (error) {
         console.error('sales_report error:', error);
         return res.status(500).json({status:message.code500,message:error.message});
