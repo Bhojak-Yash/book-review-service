549acfadfab040aac6bb1b6ce3a107b993943ef7
manjeetcoreteams <manjeet@coreteams.us>
2025-06-12 11:31:27 +0530

    changes

--- src/controllers/tallyReports.js
+++ src/controllers/tallyReports.js
@@ -23,3 +23,15 @@ exports.partywise_payable_report = async (req, res) => {
     return res.status(500).json({ status:500,message:error.message });
   }
 };
+
+exports.ladger_report = async (req, res) => {
+  try {
+    const data = {...req.query,...req.user}
+    const report = await TallyReportsService.ladger_report(data,res);
+
+    // return res.status(report?.status || 200).json(report);
+  } catch (error) {
+    console.error("Error fetching ladger_report:", error);
+    return res.status(500).json({ status:500,message:error.message });
+  }
+};
--- src/routers/tallyReports.js
+++ src/routers/tallyReports.js
@@ -4,5 +4,6 @@ const tallyReportsc = require('../controllers/tallyReports')
 
 router.get('/partywise-outstanding-report',verifyToken,tallyReportsc.partywise_outstanding_report)
 router.get('/partywise-payable-report',verifyToken,tallyReportsc.partywise_payable_report)
+router.get('/ledger-report',verifyToken,tallyReportsc.ladger_report)
 
 module.exports =router
