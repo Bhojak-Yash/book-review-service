8b1932349f940f45f05a8732453f7244274c35de
manjeetcoreteams <manjeet@coreteams.us>
2025-06-18 10:04:10 +0000

    changes

--- src/services/distributorPanelService.js
+++ src/services/distributorPanelService.js
@@ -1478,10 +1478,10 @@ class distributorDashboard {
             const date = dateString ? new Date(dateString) : new Date();
 
             const startOfDay = new Date(date.setHours(0, 0, 0, 0));
-            startOfDay.setMinutes(startOfDay.getMinutes() + 330);
+            // startOfDay.setMinutes(startOfDay.getMinutes() + 330);
 
             const endOfDay = new Date(date.setHours(23, 59, 59, 999));
-            endOfDay.setMinutes(endOfDay.getMinutes() + 330);
+            // endOfDay.setMinutes(endOfDay.getMinutes() + 330);
 
 
             let checkUserType = tokenData.userType;
